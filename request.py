f2aa90c10ba50970400f5f91c0e1dc22a4884d76
Avery <36283359+Avery-Baechler@users.noreply.github.com>
2022-08-15 06:20:33 +0000

    changing request.py

--- request.py
+++ request.py
@@ -1,24 +1,43 @@
 
+from socketserver import DatagramRequestHandler
 import requests
 import bs4
-r = requests.get('https://spotwx.com/products/grib_index.php?model=gem_lam_continental&lat=48.4179&lon=-123.35927&tz=America/Vancouver&display=table')
-body = r.content
+import ssl
+import re
+import pandas as pd
+from urllib3 import poolmanager
+
+url = 'https://spotwx.com/products/grib_index.php?model=gem_lam_continental&lat=48.4179&lon=-123.35927&tz=America/Vancouver&display=table'
+
+
+#what does all this mean
+class TLSAdapter(requests.adapters.HTTPAdapter):
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        """Create and initialize the urllib3 PoolManager."""
+        ctx = ssl.create_default_context()
+        ctx.set_ciphers('DEFAULT@SECLEVEL=1')
+        self.poolmanager = poolmanager.PoolManager(
+                num_pools=connections,
+                maxsize=maxsize,
+                block=block,
+                ssl_version=ssl.PROTOCOL_TLS,
+                ssl_context=ctx)
+
+session = requests.session()
+session.mount('https://', TLSAdapter())
+res = session.get(url)
+print(res)
+
+body = res.content.decode()
 
 #start = body.index("var aDataSet =") #2934
 #end = body.index("$(document).ready(function() {") #10257
 
-#data = body[2948:10257]
+data = body[2948:10256]
 
-soup = bs4.BeautifulSoup(body,'html.parser')
+data = re.sub("'", "\"", data)
 
-s = soup.find('div',class_='container')
+json_data = pd.read_json(data)
 
-#t = s.find('table',id='example')
-
-print(ss)
-
-
-
-
-
-
+print(json_data)
