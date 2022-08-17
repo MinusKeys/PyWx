7dc07ae7181d185caadb84d5868f00087ee5bce6
Avery <36283359+Avery-Baechler@users.noreply.github.com>
2022-08-17 02:59:25 +0000

    working on requests

--- request.py
+++ request.py
@@ -1,5 +1,7 @@
 
 from socketserver import DatagramRequestHandler
+from sys import maxsize
+from urllib import request
 import requests
 import bs4
 import ssl
@@ -11,6 +13,8 @@ url = 'https://spotwx.com/products/grib_index.php?model=gem_lam_continental&lat=
 
 
 #what does all this mean
+
+'''
 class TLSAdapter(requests.adapters.HTTPAdapter):
 
     def init_poolmanager(self, connections, maxsize, block=False):
@@ -28,7 +32,15 @@ session = requests.session()
 session.mount('https://', TLSAdapter())
 res = session.get(url)
 print(res)
+'''
+s = requests.Session()
+a = requests.adapters.HTTPAdapter(pool_maxsize = maxsize)
+s.mount('http://', a)
+res = s.get(url)
 
+
+
+'''
 body = res.content.decode()
 
 #start = body.index("var aDataSet =") #2934
@@ -41,3 +53,4 @@ data = re.sub("'", "\"", data)
 json_data = pd.read_json(data)
 
 print(json_data)
+'''
